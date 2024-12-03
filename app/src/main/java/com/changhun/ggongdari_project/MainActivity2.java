06d88b02d9b3766399165c043882a653364078f7
1bio <leeckdgns07@naver.com>
2024-12-03 11:21:21 +0900

    파일 최신화

--- app/src/main/java/com/changhun/ggongdari_project/MainActivity2.java
+++ app/src/main/java/com/changhun/ggongdari_project/MainActivity2.java
@@ -1,15 +1,39 @@
 package com.changhun.ggongdari_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private Button homeButton;
+    private Button recordButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        homeButton = findViewById(R.id.homeBtn_layout2);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recordButton = findViewById(R.id.recordBtn_layoyt_2);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
